0b491db1999416bbec8e95a2eeadb3f1205bbb8d
alexander.joey@gmail.com <learnacademy@LEARNs-MacBook-Air.local>
2023-04-04 15:33:39 -0700

    added frontend structure with components, pages, mock file and styling installs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom"
+import React, { useState } from "react"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import BeerEdit from "./pages/BeerEdit"
+import BeerIndex from "./pages/BeerIndex"
+import BeerNew from "./pages/BeerNew"
+import BeerShow from "./pages/BeerShow"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import mockBeers from "./mockBeers"
 
-function App() {
+
+const App = () => {
+  const [beers, setBeers] = useState(mockBeers)
+  console.log(beers)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/beerindex" element={<BeerIndex />} />
+      <Route path="/beershow" element={<BeerShow />} />
+      <Route path="/beernew" element={<BeerNew />} />
+      <Route path="/beeredit" element={<BeerEdit />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import '../App.css';
+
+
+const Header = () => {
+  return (
+<></>
+  )
+}
+
+export default Header
